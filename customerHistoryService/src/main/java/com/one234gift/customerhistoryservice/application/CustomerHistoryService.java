2e0fde4c71aead105e773e746bdb83f22eca28af
wodyd202 <wodyd202@daum.net>
2021-10-26 23:19:17 +0900

    retryable 추가

--- customerHistoryService/src/main/java/com/one234gift/customerhistoryservice/application/CustomerHistoryService.java
+++ customerHistoryService/src/main/java/com/one234gift/customerhistoryservice/application/CustomerHistoryService.java
@@ -6,33 +6,44 @@ import com.one234gift.customerhistoryservice.domain.model.CustomerHistoryEvent;
 import com.one234gift.customerhistoryservice.domain.read.CustomerHistoryModel;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Retryable;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @Slf4j
 @AllArgsConstructor
-@Retryable(maxAttempts = 3, include = SQLException.class, backoff = @Backoff(delay = 500))
 public class CustomerHistoryService {
     private CustomerHistoryRepository customerHistoryRepository;
+    private RetryTemplate retryTemplate;
 
     @Transactional(readOnly = true)
     public List<CustomerHistoryModel> findAll(String customerId, Pageable pageable) {
-        log.info("load customer history : {}", customerId);
-        return customerHistoryRepository.findAll(customerId, pageable);
+        List<CustomerHistoryModel> customerHistoryModel = retryTemplate.execute(retryContext -> {
+            log.info("load customer history : {}", customerId);
+            return customerHistoryRepository.findAll(customerId, pageable);
+        }, retryContext -> {
+            log.error("retry error reson : {}", retryContext.getLastThrowable());
+            return new ArrayList<>();
+        });
+        return customerHistoryModel;
     }
 
     @Transactional
     public CustomerHistoryModel save(CustomerHistoryEvent customerHistoryEvent) {
         CustomerHistory customerHistory = CustomerHistory.register(customerHistoryEvent);
-        customerHistoryRepository.save(customerHistory);
-        CustomerHistoryModel customerHistoryModel = customerHistory.toModel();
-        log.info("save customer history : {}", customerHistoryModel);
-        return customerHistoryModel;
+        return retryTemplate.execute(retryContext -> {
+            customerHistoryRepository.save(customerHistory);
+            CustomerHistoryModel customerHistoryModel = customerHistory.toModel();
+            log.info("save customer history : {}", customerHistoryModel);
+            return customerHistoryModel;
+        }, retryContext -> {
+            log.error("retry error reson : {}", retryContext.getLastThrowable());
+            return null;
+        });
     }
+
 }
--- customerHistoryService/src/main/java/com/one234gift/customerhistoryservice/config/RetryConfig.java
+++ customerHistoryService/src/main/java/com/one234gift/customerhistoryservice/config/RetryConfig.java
@@ -1,11 +1,27 @@
 package com.one234gift.customerhistoryservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 
 @EnableRetry
 @Configuration
-@Profile("!test")
 public class RetryConfig {
+
+    @Bean
+    RetryTemplate retryTemplate(){
+        RetryTemplate retryTemplate = new RetryTemplate();
+        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+        fixedBackOffPolicy.setBackOffPeriod(3000L);
+
+        SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy();
+        simpleRetryPolicy.setMaxAttempts(3);
+
+        retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+        retryTemplate.setRetryPolicy(simpleRetryPolicy);
+        return retryTemplate;
+    }
 }
